7791c9a5dd04849a084fde8a0bd657d179f3719e
abougit <abougitluke@gmail.com>
2023-04-26 21:02:27 +0200

    ajout model pix2pix

---
+++ source/script/README.md
@@ -0,0 +1,2 @@
+# AI_GAN_Comics_BD
+AI Generative Adverarial Networks for comics &amp; bd
---
+++ source/script/upscaling.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import os
+
+#Upscale Image
+from super_image import EdsrModel, ImageLoader
+from PIL import Image
+import requests
+
+# To ignore warning
+import warnings
+import sys
+
+if __name__ == '__main__':
+
+	arg  = sys.argv[1]
+	warnings.simplefilter("ignore", UserWarning)
+	with torch.no_grad():
+		model = EdsrModel.from_pretrained('eugenesiow/edsr', scale=2)
+
+		folder_path = str(arg)
+		      
+		img = Image.open(folder_path)
+
+		inputs = ImageLoader.load_image(img)
+		preds = model(inputs)
+
+		ImageLoader.save_image(preds, './outputs/scaled_2x.png')
+
